559bb62d79b0e6ae772c7ae85c6b9028a9dae686
madushanka kumara <oska.coc@gmail.com>
2020-03-11 15:28:45 +0530

    Reoperate RestController

--- src/main/java/lk/madushanka/mongo/test/mongo/controller/StudentController.java
+++ src/main/java/lk/madushanka/mongo/test/mongo/controller/StudentController.java
@@ -1,10 +1,11 @@
 package lk.madushanka.mongo.test.mongo.controller;
 
+import lk.madushanka.mongo.test.mongo.business.custom.StudentBO;
 import lk.madushanka.mongo.test.mongo.dto.StudentDTO;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.*;
 
-import java.awt.*;
 import java.util.List;
 
 @CrossOrigin("*")
@@ -12,29 +13,35 @@ import java.util.List;
 @RequestMapping("/api/v1/students")
 public class StudentController {
 
+    @Autowired
+    private StudentBO studentBO;
+
     @GetMapping(produces = MediaType.APPLICATION_JSON_VALUE)
     public List<StudentDTO> getAll(){
-        return null;
+        List<StudentDTO> all = studentBO.getAll();
+        return all;
+
     }
 
     @GetMapping(value = "/{id}",produces = MediaType.APPLICATION_JSON_VALUE)
     public StudentDTO get(@PathVariable String id){
-        return null;
+        StudentDTO one = studentBO.getOne(id);
+        return one;
     }
 
     @DeleteMapping(value = "/{id}",consumes = MediaType.APPLICATION_JSON_VALUE)
     public void delete(@PathVariable String id){
-
+        studentBO.deleteStudent(id);
     }
 
     @PostMapping(consumes = MediaType.APPLICATION_JSON_VALUE)
     public void save(@RequestBody StudentDTO student){
-
+        studentBO.saveStudent(student);
     }
 
     @PutMapping(value = "/{id}",consumes = MediaType.APPLICATION_JSON_VALUE)
     public void update(@PathVariable String id,@RequestBody StudentDTO student){
-
+        studentBO.updateStudent(student);
     }
 
 }
